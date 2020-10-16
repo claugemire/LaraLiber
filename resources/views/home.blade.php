3c5e2d4be235b4d7517ecb49a946be6ec72bfaf6
Jake McTyre <jmctyre@phase2technology.com>
2020-10-16 12:38:40 -0500

    Adding vue and verifying with new AddBook component stub

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            {{ __('You are logged in!') }}
+
+            <add-book />
+
         </div>
     </div>
 </div>
