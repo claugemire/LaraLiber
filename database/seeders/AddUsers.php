859d9e05a2776051be0cd407109e901fee771401
Jake McTyre <jmctyre@phase2technology.com>
2020-10-16 10:48:46 -0500

    Removing Livewire

---
+++ database/seeders/AddUsers.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AddUsers extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->
+            insert([
+                'name' => 'jake',
+                'email' => 'jake@jmctyre.com',
+                'password' => bcrypt('jake')
+            ]);
+
+        DB::table('users')->
+            insert([
+                'name' => 'tester',
+                'email' => 'jake+tester@jmctyre.com',
+                'password' => bcrypt('tester')
+            ]);
+    }
+}
